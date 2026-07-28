3497192cbced915ea29ec7560757e202ff763c76
bycha <bychaverridaniel@gmail.com>
2026-07-28 15:04:23 -0600

    Clase controlador
    Se crea la union con menu y con proincipiante con la clase cronometro

--- src/controlador/Juego.java
+++ src/controlador/Juego.java
@@ -4,10 +4,28 @@
  */
 package controlador;
 
+import Vista.Menu;
+import Vista.Principiante;
+import modelo.Cronometro;
+
 /**
  *
  * @author bycha
  */
 public class Juego {
+    private Cronometro modelo;
+    private Menu menu;
+
+    public Juego(Cronometro modelo, Menu menu) {
+        this.modelo = modelo;
+        this.menu = menu;
+    }
+    
+    public void MostrarCronometro(){
+        Timer tiempo = Cronometro.TxtTiempo;
+    }
+    
+    
+    
     
 }
